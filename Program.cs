88685dca3641ba076ec3e13dd972cbcb8f53ad58
MoUsE <MoUsE@MINI-ITX>
2022-04-01 19:07:05 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpOSC;
+using System.Threading;
+namespace TulaOSC
+{
+    class Program
+    {
+        private static DateTime time;
+        private static float Minutos;
+        private static float Horas;
+        
+        static void Main(string[] args)
+        {
+            getTime();
+        }
+
+        private static void getTime() {
+
+            while (true) {
+            Thread.Sleep(10000);
+            time = DateTime.Now;
+            Minutos = time.Minute;
+            Horas = time.Hour;
+            var message = new SharpOSC.OscMessage("/avatar/parameters/timeH",Horas/200);
+            var sender = new SharpOSC.UDPSender("127.0.0.1", 9000);
+            sender.Send(message);
+            message = new SharpOSC.OscMessage("/avatar/parameters/timeM",Minutos/200);
+            sender.Send(message);
+            Console.WriteLine("Enviados");
+
+            }
+        }
+    }
+}
