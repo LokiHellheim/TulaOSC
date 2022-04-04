7d7c594cd57198d1dcdd807fc470b894bf43f7da
Munrazfaust <munraz192@gmail.com>
2022-04-03 21:20:50 -0500

    I don't remember the change xd

--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SharpOSC;
 using System.Threading;
 using System.IO;
 using Newtonsoft.Json;
 using WebSocketSharp;
+
 
 namespace TulaOSC
 {
