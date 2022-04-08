cc6a8431b767385e650d4b16bae91aa4a93a24fd
MoUsE Muon tsukamoto <MoUsE_021.xD@hotmail.com>
2022-04-08 18:04:54 -0500

    GPU usage, mem gpu usage

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using SharpOSC;
 using System.Threading;
 using System.IO;
@@ -14,6 +16,8 @@ namespace TulaOSC
 
         private static PerformanceCounter perfCPUCounter;
         private static PerformanceCounter perfMemCounter;
+        private static PerformanceCounter perfGPUCounter;
+        private static PerformanceCounter perfGPUMemoryCounter;
         private static DateTime time;
         private static float minutes;
         private static float hours;
@@ -42,7 +46,17 @@ namespace TulaOSC
             while (true)
             {
 
-                perfCPUCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+                /*  TESTING GPU USAGE
+                   
+                  perfGPUCounter = new PerformanceCounter("GPU Engine", "Utilization Percentage","pid_25376_luid_0x00000000_0x0000F44A_phys_0_eng_0_engtype_3D");
+                  perfGPUCounter.NextValue();
+                 perfGPUMemoryCounter = new PerformanceCounter("GPU Adapter Memory", "Dedicated Usage", "luid_0x00000000_0x0000F44A_phys_0");
+                 Console.WriteLine("Sent GPUMemory On usage :" + (ulong)perfGPUMemoryCounter.NextValue()/1000000 + " MB");
+                 Console.WriteLine("Sent GPU Usage % :" + (int)perfGPUCounter.NextValue());*/
+                perfCPUCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+                perfCPUCounter.NextValue();
+                Thread.Sleep(1000);
+
                 perfMemCounter = new PerformanceCounter("Memory", "Available MBytes");
                 time = DateTime.Now;
                 minutes = time.Minute;
@@ -53,8 +67,18 @@ namespace TulaOSC
                 message = new OscMessage("/avatar/parameters/timeM", minutes / 200);
                 sender.Send(message);
                 Console.WriteLine("Sent: " + hours + ":" + minutes);
-                Console.WriteLine((int)perfCPUCounter.NextValue());
-                Console.WriteLine((int)perfMemCounter.NextValue());
+                Console.WriteLine("Sent Cpu Usage % :" + (int)perfCPUCounter.NextValue()+" %");
+                Console.WriteLine("Sent Memory Free :" + Math.Round((int)perfMemCounter.NextValue()*.0001,3) + " GB");
+                var gpuCounters = GetGPUCounters();
+                var gpuUsage = GetGPUUsage(gpuCounters);
+                var gpuMEMCounters = GetGPUMEMCounters();
+                var gpuMEMUsage = GetGPUMEMUsage(gpuMEMCounters);
+                Console.WriteLine("Sent GPU usage : " + Math.Round(gpuUsage,0)+ " %");
+                Console.WriteLine("Sent GPU MEM : " + Math.Round(gpuMEMUsage*.000000001,2)+" GB");
+
+
+
+
                 Thread.Sleep(10000);
             }
         }
@@ -65,20 +89,22 @@ namespace TulaOSC
                 var ws = new WebSocket(wsUrl);
                 ws.SslConfiguration.EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12;
                 ws.OnMessage += OnMessageWS;
-               
+
                 ws.Connect();
-                
-            }catch (Exception ex)
+
+            }
+            catch (Exception ex)
             { Console.WriteLine(ex.ToString()); }
 
-           
+
         }
-        
+
         private static void OnMessageWS(object sender, MessageEventArgs e)
         {
-            if(aTimer != null){
+            if (aTimer != null)
+            {
                 aTimer.Stop();
-                aTimer.Dispose();   
+                aTimer.Dispose();
             }
             jsonStromno m = JsonConvert.DeserializeObject<jsonStromno>(e.Data.ToString());
             Console.WriteLine("Heart Rate : " + m.data.heartRate);
@@ -108,6 +134,50 @@ namespace TulaOSC
             var message = new OscMessage("/avatar/parameters/hr_connected", false);
             var s = new UDPSender(localhost, port);
             s.Send(message);
+        }
+
+        public static List<PerformanceCounter> GetGPUCounters()
+        {
+            var category = new PerformanceCounterCategory("GPU Engine");
+            var counterNames = category.GetInstanceNames();
+
+            var gpuCounters = counterNames
+                                .Where(counterName => counterName.EndsWith("engtype_3D"))
+                                .SelectMany(counterName => category.GetCounters(counterName))
+                                .Where(counter => counter.CounterName.Equals("Utilization Percentage"))
+                                .ToList();
+
+            return gpuCounters;
+        }
+        public static float GetGPUUsage(List<PerformanceCounter> gpuCounters)
+        {
+            gpuCounters.ForEach(x => x.NextValue());
+            Thread.Sleep(1000);
+            var result = gpuCounters.Sum(x => x.NextValue());
+
+            return result;
+        }
+
+        public static List<PerformanceCounter> GetGPUMEMCounters()
+        {
+            var category = new PerformanceCounterCategory("GPU Adapter Memory");
+            var counterNames = category.GetInstanceNames();
+
+            var gpuCounters = counterNames
+                                .Where(counterName => counterName.EndsWith("A_phys_0"))
+                                .SelectMany(counterName => category.GetCounters(counterName))
+                                .Where(counter => counter.CounterName.Equals("Total Committed"))
+                                .ToList();
+
+            return gpuCounters;
+        }
+        public static float GetGPUMEMUsage(List<PerformanceCounter> gpuCounters)
+        {
+            gpuCounters.ForEach(x => x.NextValue());
+            Thread.Sleep(1000);
+            var result = gpuCounters.Sum(x => x.NextValue());
+
+            return result;
         }
     }
 
